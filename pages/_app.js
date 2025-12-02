93db2088116e9aa16260e75c2bbea34dc3e310e5
siva_ratnakar <91734819+siva-ratnakar@users.noreply.github.com>
2025-12-02 03:41:43 +0100

    implemented PWA support with next-pwa and webpack fix

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="./assets/icons/final_logo_small.svg" type="image/svg+xml" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#0f172a" />
+        <meta name="description" content="Sai Europe Alumni Meet 2026" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/assets/icons/final_logo_small.svg" />
+        <link rel="icon" href="/assets/icons/final_logo_small.svg" type="image/svg+xml" />
       </Head>
       <Component {...pageProps} />
     </>
